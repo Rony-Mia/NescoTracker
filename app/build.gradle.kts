677cb0730fbd6b4ac7789b0f08257d6a2e44c43f
rony-mia <ronymia2021@gmail.com>
2026-07-02 19:58:22 +0600

    Bump version to 2.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.nescotracker"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 7
+        versionName = "2.7"
     }
 
     buildTypes {
